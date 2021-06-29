f335c7521ffca4890aa95a2e82521b060b90665c
Wang Yang <1527638985@qq.com>
2021-06-29 10:16:45 +0800

    Index redo complete

--- EADPPROJ/MasterPage.Master.designer.cs
+++ EADPPROJ/MasterPage.Master.designer.cs
@@ -15,13 +15,13 @@ namespace EADPPROJ
     {
 
         /// <summary>
-        /// profileUrl 控件。
+        /// appGroup 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor profileUrl;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl appGroup;
 
         /// <summary>
         /// profileimg 控件。
@@ -33,13 +33,31 @@ namespace EADPPROJ
         protected global::System.Web.UI.HtmlControls.HtmlImage profileimg;
 
         /// <summary>
-        /// appGroup 控件。
+        /// login_func 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl appGroup;
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor login_func;
+
+        /// <summary>
+        /// register_func 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor register_func;
+
+        /// <summary>
+        /// profileUrl 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor profileUrl;
 
         /// <summary>
         /// logout 控件。
@@ -49,24 +67,6 @@ namespace EADPPROJ
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlAnchor logout;
-
-        /// <summary>
-        /// login_func 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl login_func;
-
-        /// <summary>
-        /// register_func 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl register_func;
 
         /// <summary>
         /// form1 控件。
